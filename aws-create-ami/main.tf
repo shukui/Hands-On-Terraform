9669d8dbbaa7fcf65ddf666ac4dfdbc06769e361
shukui <shookui@gmail.com>
2023-05-01 08:40:03 +0200

    first commit

---
+++ aws-create-ami/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key =""
+    secret_key = ""
+}
+
+resource "aws_vpc" "first-vpc" {    
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.first-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
