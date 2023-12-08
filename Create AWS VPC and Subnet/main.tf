c45caf6906c7db062b6966b60b35f455eefe42a9
Shukui Zhang <shookui@gmail.com>
2023-12-08 20:22:36 +0100

    Update main.tf

--- Create AWS VPC and Subnet/main.tf
+++ Create AWS VPC and Subnet/main.tf
@@ -7,6 +7,7 @@ variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
 variable my_ip {}
+variable instance_type {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -91,4 +92,26 @@ resource "aws_security_group" "myapp-sg"  {
   tags = {
     Name: "${var.env_prefix}-sg"
   }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image
+}
+
+resource  "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = 
 }
