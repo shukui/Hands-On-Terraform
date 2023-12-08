ab95f2de70c085e19d0e25272af0eb22986c918a
Shukui Zhang <shookui@gmail.com>
2023-12-08 20:00:56 +0100

    Add igress and egress

--- Create AWS VPC and Subnet/main.tf
+++ Create AWS VPC and Subnet/main.tf
@@ -72,4 +72,19 @@ resource "aws_security_group" "myapp-sg"  {
     protocol = "tcp"
     cidr_block = [var.my-ip]
   }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_block = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_block = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
 }
