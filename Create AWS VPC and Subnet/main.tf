b404f7f9381f29fc3fed0a99d4c21cabda7984f9
Shukui Zhang <shookui@gmail.com>
2023-12-08 21:22:28 +0100

    key pair

--- Create AWS VPC and Subnet/main.tf
+++ Create AWS VPC and Subnet/main.tf
@@ -111,9 +111,13 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image
 }
 
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
 resource "aws_key_pair" "ssh-key" {
   key_name = "server-key"
-  public_key = "${file(var.public_key_location)}"
+  public_key = file(var.public_key_location)
 }
 
 resource  "aws_instance" "myapp-server" {
@@ -125,7 +129,7 @@ resource  "aws_instance" "myapp-server" {
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
-  key_name = "server-key-pair"
+  key_name = aws_key_pair.ssh-key.key_name
 
   tags {
     Name = "${var.env_prefix}-server"
