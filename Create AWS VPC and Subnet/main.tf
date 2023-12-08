da4055d4705b7d16680b7212eeac8e262997c8be
Shukui Zhang <shookui@gmail.com>
2023-12-08 20:03:31 +0100

    Update main.tf cidr_blocks not cidr_block

--- Create AWS VPC and Subnet/main.tf
+++ Create AWS VPC and Subnet/main.tf
@@ -70,21 +70,21 @@ resource "aws_security_group" "myapp-sg"  {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_block = [var.my_ip]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
     from_port = 8080
     to_port = 8080
     protocol = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port = 0
     to_port = 0
     protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
     prefix_list_ids = []
   }
 
