b8ddaeb4cfa26ac21160cd56654b0def704dcf4c
Shukui Zhang <shookui@gmail.com>
2023-12-08 21:07:53 +0100

    key pair

--- Create AWS VPC and Subnet/main.tf
+++ Create AWS VPC and Subnet/main.tf
@@ -111,7 +111,23 @@ output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image
 }
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = "${file(var.public_key_location)}"
+}
+
 resource  "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
-  instance_type = 
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+  key_name = "server-key-pair"
+
+  tags {
+    Name = "${var.env_prefix}-server"
+  }
 }
